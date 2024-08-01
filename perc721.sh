77dca23bef27b9b48997c57f871c7d2e7503d1b6
Pankaj <65181290+noob-hacks@users.noreply.github.com>
2024-08-01 08:07:22 +0000

    feat: initiated the project

--- perc721.sh
+++ perc721.sh
@@ -6,7 +6,7 @@ sleep 4
 sudo apt-get update && sudo apt-get upgrade -y
 clear
 
-echo "Installing Hardhat and dotenv..."
+echo "Installing dependencies..."
 npm install --save-dev hardhat
 npm install dotenv
 npm install @swisstronik/utils
@@ -47,30 +47,49 @@ module.exports = {
 EOL
 echo "Hardhat configuration completed."
 
-read -p "Enter the token name: " TOKEN_NAME
-read -p "Enter the token symbol: " TOKEN_SYMBOL
+read -p "Enter the NFT name: " NFT_NAME
+read -p "Enter the NFT symbol: " NFT_SYMBOL
 
-echo "Creating Token.sol contract..."
+echo "Creating PrivateNFT.sol contract..."
 mkdir -p contracts
-cat <<EOL > contracts/Token.sol
-// SPDX-License-Identifier: UNLICENSED
+cat <<EOL > contracts/PrivateNFT.sol
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestToken is ERC20 {
-    constructor()ERC20("$TOKEN_NAME","$TOKEN_SYMBOL"){} 
+contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC721("$NFT_NAME","$NFT_SYMBOL")
+        Ownable(initialOwner)
+    {}
 
-    function mint100tokens() public {
-        _mint(msg.sender, 100*10**18);
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
     }
 
-    function burn100tokens() public{
-        _burn(msg.sender, 100*10**18);
+    function balanceOf(address owner) public view override returns (uint256) {
+        require(msg.sender == owner, "PrivateNFT: msg.sender != owner");
+        return super.balanceOf(owner);
+    }
+
+    function ownerOf(uint256 tokenId) public view override returns (address) {
+        address owner = super.ownerOf(tokenId);
+        require(msg.sender == owner, "PrivateNFT: msg.sender != owner");
+        return owner;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        address owner = super.ownerOf(tokenId);
+        require(msg.sender == owner, "PrivateNFT: msg.sender != owner");
+        return super.tokenURI(tokenId);
     }
 }
 EOL
-echo "Token.sol contract created."
+echo "PrivateNFT.sol contract created."
 
 echo "Compiling the contract..."
 npx hardhat compile
@@ -83,7 +102,9 @@ const hre = require("hardhat");
 const fs = require("fs");
 
 async function main() {
-  const contract = await hre.ethers.deployContract("TestToken");
+  const [deployer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("PrivateNFT");
+  const contract = await contractFactory.deploy(deployer.address);
   await contract.waitForDeployment();
   const deployedContract = await contract.getAddress();
   fs.writeFileSync("contract.txt", deployedContract);
@@ -121,17 +142,17 @@ const sendShieldedTransaction = async (signer, destination, data, value) => {
 async function main() {
   const contractAddress = fs.readFileSync("contract.txt", "utf8").trim();
   const [signer] = await hre.ethers.getSigners();
-  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contractFactory = await hre.ethers.getContractFactory("PrivateNFT");
   const contract = contractFactory.attach(contractAddress);
-  const functionName = "mint100tokens";
-  const mint100TokensTx = await sendShieldedTransaction(
+  const functionName = "safeMint";
+  const safeMintTx = await sendShieldedTransaction(
     signer,
     contractAddress,
-    contract.interface.encodeFunctionData(functionName),
+    contract.interface.encodeFunctionData(functionName, [signer.address, 1]),
     0
   );
-  await mint100TokensTx.wait();
-  console.log("Transaction Receipt: ", \`Minting token has been success! Transaction hash: https://explorer-evm.testnet.swisstronik.com/tx/\${mint100TokensTx.hash}\`);
+  await safeMintTx.wait();
+  console.log("Transaction Receipt: ", \`Minting NFT has been success! Transaction hash: https://explorer-evm.testnet.swisstronik.com/tx/\${safeMintTx.hash}\`);
 }
 
 main().catch((error) => {
@@ -141,53 +162,8 @@ main().catch((error) => {
 EOL
 echo "mint.js script created."
 
-echo "Minting tokens..."
+echo "Minting NFT..."
 npx hardhat run scripts/mint.js --network swisstronik
-echo "Tokens minted."
+echo "NFT minted."
 
-echo "Creating transfer.js script..."
-cat <<EOL > scripts/transfer.js
-const hre = require("hardhat");
-const fs = require("fs");
-const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
-
-const sendShieldedTransaction = async (signer, destination, data, value) => {
-  const rpcLink = hre.network.config.url;
-  const [encryptedData] = await encryptDataField(rpcLink, data);
-  return await signer.sendTransaction({
-    from: signer.address,
-    to: destination,
-    data: encryptedData,
-    value,
-  });
-};
-
-async function main() {
-  const contractAddress = fs.readFileSync("contract.txt", "utf8").trim();
-  const [signer] = await hre.ethers.getSigners();
-  const contractFactory = await hre.ethers.getContractFactory("TestToken");
-  const contract = contractFactory.attach(contractAddress);
-  const functionName = "transfer";
-  const amount = 1 * 10 ** 18;
-  const functionArgs = ["0x16af037878a6cAce2Ea29d39A3757aC2F6F7aac1", amount.toString()];
-  const transaction = await sendShieldedTransaction(
-    signer,
-    contractAddress,
-    contract.interface.encodeFunctionData(functionName, functionArgs),
-    0
-  );
-  await transaction.wait();
-  console.log("Transaction Response: ", \`Transfer token has been success! Transaction hash: https://explorer-evm.testnet.swisstronik.com/tx/\${transaction.hash}\`);
-}
-
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
-EOL
-echo "transfer.js script created."
-
-echo "Transferring tokens..."
-npx hardhat run scripts/transfer.js --network swisstronik
-echo "Tokens transferred."
 echo "Done! Subscribe: https://t.me/GaCryptOfficial"
